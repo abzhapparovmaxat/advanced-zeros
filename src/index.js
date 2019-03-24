d0bc6c337e441d774a65a1f8e6afe17115a55d6d
abzhapparovmaxat <abzhapparovmaxat@gmail.com>
2019-03-24 23:06:42 +0600

    fix: function getZerosCount in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 module.exports = function getZerosCount(number, base) {
   // your implementation
+  const countersArray = [];
+  const dividersArray = [];
+  let counter = 0;
+  let j = 0;
+  
+  for (i = 2; i <= base; i++) {
+  	while(base%i === 0) {
+      dividersArray[j] = i;
+      counter++;
+      countersArray[j] = counter;
+      base /= i;
+  		if(base%i !== 0) {
+        j++;
+        counter = 0; 
+      }
+    }    
+  }
+
+  const amountsArray = [];
+  let amount = 0;
+
+  for(i = 0; i < dividersArray.length; i++) {
+    j = 1;
+    amount = 0;
+  	while(number >= Math.pow(dividersArray[i], j)) {
+      amount += Math.floor(number / Math.pow(dividersArray[i], j)) ;
+  		j++;
+  	}
+    amountsArray[i] = Math.floor(amount / countersArray[i]);    
+  }
+  
+  return Math.min.apply(null, amountsArray);
 }
